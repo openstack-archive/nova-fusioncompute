993b7e00b701e3d56cfc2261449511d9026edf90
DongZhi <517341003@qq.com>
2016-10-20 12:16:24 +0800

    Initial Load of Nova FusionCompute Project
    Initial work to enable the Nova FusionCompute project in git.
    The work done here provides:
        - .gitignore - Indicate which files not to track within Git.
    	- .gitreview - Input to the git-review command on how to send to Gerrit.
    	- .testr.conf - Conf file input for the testr command (UT)
    	- CONTRIBUTING.rst - Information on how to contribute.
    	- HACKING.rst - Information on what needs to be done for updates.
    	- LICENSE - The license for the project - README.rst
    	- Information on what this project is. Currently this is the blueprint.
    	- openstack-common.conf - Required openstack configuration for all projects
    	- setup.cfg - Input to the setup.py on how to execute certain actions.
    	- setup.py - Used for build of the project. - requirements.txt
    	- Required packages (and levels) to run the code.
    	- test-requirements.txt
    	- Required packages (and levels) in addition to the requirements, that indicates what is needed to run the UT.
    	- tox.ini - The input for the tox commands. In addition, a base set of packages for the agent and unit tests were loaded in.
    
    Change-Id: Id76684afa9c8617b40e8b175785f94ce7fb9a1d6

---
+++ nova/virt/fusioncomputeapi/ops_base.py
@@ -0,0 +1,92 @@
+# Copyright 2016 Huawei Technologies Co.,LTD.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+class OpsBase(object):
+    """
+    fc operation base class
+    """
+
+    def set_client(self, fc_client):
+        """
+        set client obj
+        :param fc_client:
+        :return:
+        """
+        self.fc_client = fc_client
+        if self.fc_client:
+            self.site = self.fc_client.context
+        else:
+            self.site = None
+
+    def __init__(self, fc_client):
+        self.fc_client = None
+        self.site = None
+        self.set_client(fc_client)
+
+    @property
+    def site_id(self):
+        """
+        get site id
+        :return:
+        """
+        return self.site['site_id']
+
+    def get_path_by_site(self, path=None, **kwargs):
+        """
+        get rest path by site
+        :param path:
+        :param kwargs:
+        :return:
+        """
+        return self.site.get_path_by_site(path, **kwargs)
+
+    def post(self, path, data=None, **kwargs):
+        """
+            Post.
+        :param path: path under Context, something like '/app/resource'
+        :param data: (Optional) data of request
+        :param kwargs: headers, etc.
+        :return: Response object in requests
+        """
+        return self.fc_client.post(path, data=data, **kwargs)
+
+    def get(self, path, **kwargs):
+        """
+            Get.
+        :param path: path under Context, something like '/app/resource/id'
+        :param kwargs:  headers, etc.
+        :return: Response object in requests
+        """
+        return self.fc_client.get(path, **kwargs)
+
+    def put(self, path, data=None, **kwargs):
+        """
+            Put.
+        :param path: path under Context, something like '/app/resource/id'
+        :param data: (Optional) data of request
+        :param kwargs: headers, etc.
+        :return: Response object in requests
+        """
+        return self.fc_client.put(path, data=data, **kwargs)
+
+    def delete(self, path, **kwargs):
+        """
+            Delete.
+        :param path: path under Context, something like '/app/resource/id'
+        :param kwargs: headers, etc.
+        :return: Response object in requests
+        """
+        return self.fc_client.delete(path, **kwargs)
